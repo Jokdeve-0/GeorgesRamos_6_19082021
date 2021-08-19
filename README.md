ef79423da8face0c8fac4161a45ff4039a6d3197
Jokdeve <81690753+Jokdeve-0@users.noreply.github.com>
2021-08-19 22:21:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GeorgesRamos_6_19082021
+ Projet 6 openclassrooms
---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// create an express app
+const app = express()
+
+// get the path with 'path' module
+const path = require('path')
+
+// import routers for requests for user and sauce
+const userRoutes = require('./routes/user')
+const sauceRoutes = require('./routes/sauce')
+
+// connection to the database
+mongoose.connect('mongodb+srv://jokdeve:Jokarts91@form.vftzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Successful database connection !'))
+  .catch(() => console.log('Failed database connection !'))
+
+// parse incoming request bodies in a middleware before your handlers, available under the req.body property
+app.use(express.json())
+
+// defines custom response headers. Changes to the headers occur synchronously. 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+
+// define the routes with his router
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/api/sauces',sauceRoutes)
+app.use('/api/auth',userRoutes)
+
+module.exports = app
